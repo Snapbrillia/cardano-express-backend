b9a200566325f2154f0d76d25ae1b86016d76667
gandalf <tasos.valtinos@gmail.com>
2022-08-02 00:22:44 +0800

    Added function to query utxos of our script

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const utxoRouter = require('./routes/utxos')
 const app = express();
+
+app.set("view_engine", "ejs");
+
+app.use("/utxos", utxoRouter);
 
 app.listen(8000, () => {
     console.log("App is running port 8000")

3819b84241c0d0a7eadba1a0391d924aecd827d0
Shan Zhang <yourgitemail@example.com>
2022-08-12 22:20:33 -0400

    fixed previous changes

--- bashScripts/wallet-gen.sh
+++ bashScripts/wallet-gen.sh
@@ -1,7 +1,13 @@
+. /Users/shanzhang/cardano-express-backend/bashScripts/env.sh
+
+
 MAGIC="--testnet-magic 1097911063"
 
 cardano-cli address key-gen --verification-key-file WalletsKeys/$1.vkey --signing-key-file WalletsKeys/$1.skey
 
 cardano-cli address build --payment-verification-key-file WalletsKeys/$1.vkey --out-file WalletsKeys/$1.addr $MAGIC
+
+vkey_to_public_key_hash WalletsKeys/$1.vkey WalletsKeys/$1.pkh
+
 
 cat "WalletsKeys/$1.addr"

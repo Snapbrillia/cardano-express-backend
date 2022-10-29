fd7c69f9d23deaf5650430fb8c07829e35c21eb2
Shan Zhang <shanzhang@Shans-MacBook-Air.local>
2022-10-29 01:01:28 -0400

    add donation api

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -12,8 +12,8 @@ const generateGrantTx = async (req, res) => {
     } = req.body;
     exec(
       "source " +
-        "/Users/shanzhang/quadraticVoting/scripts/register-project.sh" +
-        " " +
+        __dirname +
+        "../quadraticVoting/scripts/register-project.sh " +
         projectLabel +
         " " +
         requestedAmount +
@@ -26,21 +26,57 @@ const generateGrantTx = async (req, res) => {
         " " +
         txOut,
       (err, stdout, stderr) => {
-        if (err) {
-          console.log(err);
+        if (stdout) {
+          res.json({ stdout });
+        } else {
+          return res.status(500).json({ error: "Failed to build transaction" });
         }
-        if (stderr) {
-          console.log(stderr);
-        }
-        return res.json({ stdout });
       }
     );
   } catch (err) {
-    console.log(err);
+    return res.status(500).json({ err: err });
+  }
+};
+
+const generateDonateTx = async (req, res) => {
+  try {
+    const {
+      walletAddress,
+      pubKeyAddress,
+      projectTokenName,
+      donationAmount,
+      txIn,
+      txOut,
+    } = req.body;
+    exec(
+      "source " +
+        __dirname +
+        "../quadraticVoting/scripts/donate-to-project.sh " +
+        projectTokenName +
+        " " +
+        donationAmount +
+        " " +
+        pubKeyAddress +
+        " " +
+        walletAddress +
+        " " +
+        txIn +
+        " " +
+        txOut,
+      (err, stdout, stderr) => {
+        if (stdout) {
+          res.json({ stdout });
+        } else {
+          return res.status(500).json({ error: "Failed to build transaction" });
+        }
+      }
+    );
+  } catch (err) {
     return res.status(500).json({ err: err });
   }
 };
 
 module.exports = {
   generateGrantTx,
+  generateDonateTx,
 };

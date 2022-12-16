3fd9b53b0cb6d5c5d99c2f82f262ab8e3ccc3b05
Shan Zhang <shanzhang@Shans-MacBook-Air.local>
2022-12-16 15:39:11 -0500

    add path to repo as env variable when invoking shell scripts

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 
 app.use(express.json());
 
+require("dotenv").config();
+
 app.set("view_engine", "ejs");
 require("./routes/transaction.routes")(app);
 
-app.listen(8000, () => {
+app.listen(8001, () => {
   console.log("App is running port 8000");
 });
